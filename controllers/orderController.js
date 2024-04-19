82f3ec0018fc4ebbebb0037270fefd6085161e32
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2024-04-20 01:02:18 +0500

    111

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -16,7 +16,7 @@ export const sendMessageTg = async (req, res) => {
         }
 
         let totalCost = 0
-        let message = `<b style={{color:"red"}}>${user._doc.role === 'user' ? 'Розница' : 'ОПТ'}</b>\n`
+        let message = `<b>${user._doc.role === 'user' ? 'Розница' : 'ОПТ'}</b>\n`
         message += `<b>Статус клиента: </b>${user._doc.role === 'user' ? 'Розница' : 'ОПТ'}\n`
         message += `<b>Клиент: </b>${user._doc.name}\n`
         message += `<a href="tel:${user._doc.phoneNumber}">Номер телефона: </a>${user._doc.phoneNumber}\n`
@@ -24,6 +24,7 @@ export const sendMessageTg = async (req, res) => {
         message += `<b>Способ оплаты: </b>${req.body.paymentMethod}\n`
         message += `\n`
         message += `<b>ЗАКАЗ</b>\n`
+        message += `\n`
         basket.map((product, index) => {
             message += `${product.name} (${product.roast})\n`
             message += `Вес:${product.weight}\n`
