b82b67450a173042705eb17b54fe5638f8420ef6
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2024-05-17 18:55:54 +0500

    111

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.patch('/unlock-user',checkAuthAdmin, UserController.unlock)
 
 
 //Разработка
-app.post('/send-code',checkAuth , OrderController.sendCode)
+// app.post('/send-code',checkAuth , OrderController.sendCode)
 
 
 app.get('/get-all-managers',async(req,res)=>{
