3f3415976fc8fd412a7ea10fa25f257e54553787
Kadyrzhan23 <112863599+Kadyrzhan23@users.noreply.github.com>
2024-08-06 23:15:48 +0500

    111

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
     }
 });
 
-app.listen(process.env.PORT || 4444, (err) => {
+sslServer.listen(process.env.PORT || 4444, (err) => {
     if (err) {
         return console.log(err)
     }
